413f7bf876b4c5312a132dc712d528081831fa01
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-12-23 10:58:59 +0100

    Finish day 5

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,7 @@
+# The Gauss Challenge
+
+gauss_sum = 0
+for num in range(1,101):
+    gauss_sum += num
+
+print(gauss_sum)
--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,18 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+password = ""
 
+for i in range(nr_letters):
+    password += random.choice(letters)
+
+for i in range(nr_symbols):
+    password += random.choice(symbols)
+
+for i in range(nr_numbers):
+    password += random.choice(numbers)
+
+# create a list from string, then shuffle the list, then join the characters to recreate string
+char_list = list(password)
+random.shuffle(char_list)
+print(''.join(char_list))
