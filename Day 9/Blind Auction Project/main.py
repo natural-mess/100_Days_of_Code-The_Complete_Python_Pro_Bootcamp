bd2a053d29c43fcace5267ceb64ab7ac5c6893eb
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-12-24 15:51:52 +0100

    Finish day 9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -3,4 +3,23 @@
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+import art
 
+print(art.logo)
+
+continue_bid = "yes"
+
+data_dict = {}
+
+while continue_bid == "yes":
+    user = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    data_dict[user] = bid
+
+    continue_bid = input("Are there any other bidders? Type \'yes\' or \'no\'.\n").lower()
+
+    print("\n" * 20)
+
+highest_bid_user = max(data_dict, key=data_dict.get)
+
+print(f"The winner is {highest_bid_user} with a bid of ${data_dict[highest_bid_user]}")
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,26 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
+print(programming_dictionary["Bug"])
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for key in student_scores:
+    if student_scores[key] >= 91 and student_scores[key] <= 100:
+        student_grades[key] = "Outstanding"
+    elif student_scores[key] >= 81 and student_scores[key] <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif student_scores[key] >= 71 and student_scores[key] <= 80:
+        student_grades[key] = "Acceptable"
+    elif student_scores[key] <= 70:
+        student_grades[key] = "Fail"
