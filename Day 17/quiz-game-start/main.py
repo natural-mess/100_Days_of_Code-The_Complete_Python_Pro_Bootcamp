a2bbbf993283727d607290bdcdeb2b27738d43b1
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2026-01-05 22:02:32 +0100

    Finish day 17

---
+++ Day 17/quiz-game-start/main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    # question_bank.append(Question(question["text"], question["answer"]))
+    question_bank.append(Question(question["question"], question["correct_answer"]))
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
