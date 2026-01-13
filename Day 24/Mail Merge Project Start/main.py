cf39a023508a1118e4ef5d1f3ae1d1c9f4b731d7
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2026-01-13 21:51:34 +0100

    Finish day 24

--- Day 24/Mail Merge Project Start/main.py
+++ Day 24/Mail Merge Project Start/main.py
@@ -6,3 +6,13 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./input/Letters/starting_letter.txt") as starting_letter:
+    starting_letter_content = starting_letter.read()
+    with open("./input/Names/invited_names.txt") as invited_names:
+        list_of_names = invited_names.readlines()
+        for name in list_of_names:
+            new_name = name.strip()
+            with open(f"./Output/ReadyToSend/letter_for_{new_name}.txt", "w") as new_letter:
+                new_letter_content = starting_letter_content.replace("[name]", new_name)
+                new_letter.write(new_letter_content)
