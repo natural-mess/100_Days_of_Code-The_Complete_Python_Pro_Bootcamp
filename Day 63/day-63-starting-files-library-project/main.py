a54a6e04a02511350c19cead1591cf4673520b8d
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2026-07-14 12:05:49 +0200

    Finish day 63

--- Day 63/day-63-starting-files-library-project/main.py
+++ Day 63/day-63-starting-files-library-project/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 '''
 Red underlines? Install the required packages first: 
@@ -13,27 +16,66 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
 app = Flask(__name__)
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Book-collection.db"
+
+# initialize the app with the extension
+db.init_app(app)
 
 all_books = []
 
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return render_template("index.html", books=all_books)
+    books = db.session.execute(db.select(Book).order_by(Book.title)).scalars().all()
+    return render_template("index.html", books=books)
 
 
 @app.route("/add", methods=["POST", "GET"])
 def add():
     if request.method == 'POST':
-        all_books.append({
-            "title": request.form['book'],
-            "author": request.form['author'],
-            "rating": request.form['rating'],
-        })
+        book = Book(
+            title= request.form['book'],
+            author= request.form['author'],
+            rating= request.form['rating'],
+        )
+        db.session.add(book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
 
+@app.route("/edit", methods=["POST", "GET"])
+def edit_rating():
+    book_id = request.args.get("id", type=int)
+    book = db.get_or_404(Book, book_id)
+    if request.method == 'POST':
+        book.rating = float(request.form["rating"])
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit.html", book=book)
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get("id", type=int)
+    book = db.get_or_404(Book, book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
