52093efd46f6ba4f2e468ea7c1317ab71e9a884c
natural-mess <37369211+natural-mess@users.noreply.github.com>
2026-07-23 23:20:01 +0200

    Finish day 68

--- Day 68/day-68-starting-files-flask-auth/main.py
+++ Day 68/day-68-starting-files-flask-auth/main.py
@@ -19,10 +19,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    # This queries your database to find the user by their ID
+    return db.get_or_404(User, user_id)
+
 # CREATE TABLE IN DB
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
@@ -32,39 +40,66 @@ class User(db.Model):
 with app.app_context():
     db.create_all()
 
-
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_active)
 
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
     if request.method == "POST":
+        login_info = db.session.execute(db.select(User).filter_by(email=request.form.get('email'))).scalar()
+        if login_info:
+            flash('You have already signed up with that email, login instead!')
+            return redirect(url_for('login'))
         new_user = User(
-            email = request.form.get('title'),
-            password = request.form.get('subtitle'),
+            email = request.form.get('email'),
+            password = generate_password_hash(request.form.get('password'), method="pbkdf2:sha256", salt_length=8),
             name = request.form.get('name'),
         )
+        # print(new_user.password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('secrets'))
+        # Log in and authenticate user after adding details to database.
+        login_user(new_user)
+        return redirect(url_for('secrets'), logged_in=current_user.is_active)
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def login():
+    if request.method == "POST":
+        login_info = db.session.execute(db.select(User).filter_by(email=request.form.get('email'))).scalar()
+        print(login_info)
+        if login_info:
+            if check_password_hash(login_info.password, request.form.get('password')):
+                login_user(login_info)
+                # Imagine a user tries to visit /admin_dashboard, but they aren't logged in. 
+                # Flask-Login intercepts them and kicks them to the /login page. 
+                # But Flask is polite! It remembers where they wanted to go by putting it in the URL like this:
+                # yourwebsite.com/login?next=/admin_dashboard
+                # next = request.args.get('next')
+                return redirect(url_for('secrets'), logged_in=current_user.is_active)
+            else:
+                flash('Password incorrect, please try again.')
+                return render_template("login.html")
+        else:
+            flash('That email does not exist, please try again.')
+            return render_template("login.html")
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
